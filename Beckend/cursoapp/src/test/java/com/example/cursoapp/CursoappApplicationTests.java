bb123a40cd068dd37b46cca81daca06e919bf786
MiguelRRJose <00095322@uca.edu.sv>
2026-06-11 12:24:16 -0600

    :memo:[CREATE]Configuracion inicial del proyecto

---
+++ Beckend/cursoapp/src/test/java/com/example/cursoapp/CursoappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cursoapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
