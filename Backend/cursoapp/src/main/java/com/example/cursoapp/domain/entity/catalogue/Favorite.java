53d8e7d88a20ae4937069291a30821e806ec0c5b
MauricioMontes-UCA <00042823@uca.edu.sv>
2026-06-19 15:18:31 -0600

    :sparkles: Agregadas entidades Favorite y Review

--- Backend/cursoapp/src/main/java/com/example/cursoapp/domain/entity/catalogue/Favorite.java
+++ Backend/cursoapp/src/main/java/com/example/cursoapp/domain/entity/catalogue/Favorite.java
@@ -1,1 +1,27 @@
 package com.example.cursoapp.domain.entity.catalogue;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "favorite")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Favorite {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "course_id")
+    private UUID courseId;
+}
--- Backend/cursoapp/src/main/java/com/example/cursoapp/domain/entity/catalogue/Review.java
+++ Backend/cursoapp/src/main/java/com/example/cursoapp/domain/entity/catalogue/Review.java
@@ -1,1 +1,36 @@
 package com.example.cursoapp.domain.entity.catalogue;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.time.Instant;
+import java.util.UUID;
+
+@Entity
+@Table(name = "review")
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class Review {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(name = "score")
+    private Integer score;
+
+    @CreationTimestamp
+    @Column(name = "created_at", updatable = false)
+    private Instant createdAt;
+
+    @Column(name = "user_id")
+    private UUID userId;
+
+    @Column(name = "course_id")
+    private UUID courseId;
+}
