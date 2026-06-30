88d3c56f0d7547f3fb88c37ff2adab3ea17c19e7
MauricioMontes-UCA <00042823@uca.edu.sv>
2026-06-30 14:12:49 -0600

    :construction: Algunos arreglos para poder obtener el JWT context, faltan pruebas.

--- Backend/cursoapp/src/main/java/com/example/cursoapp/service/identity/impl/UsuarioDetailsService.java
+++ Backend/cursoapp/src/main/java/com/example/cursoapp/service/identity/impl/UsuarioDetailsService.java
@@ -1,16 +1,13 @@
 package com.example.cursoapp.service.identity;
 
+
 import com.example.cursoapp.domain.entity.identity.Usuario;
 import com.example.cursoapp.repository.identity.UsuarioRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -23,10 +20,6 @@ public class UsuarioDetailsService implements UserDetailsService {
         Usuario usuario = usuarioRepository.findByUsername(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found: " + username));
 
-        return new User(
-                usuario.getUsername(),
-                usuario.getPasswordHash(),
-                List.of(new SimpleGrantedAuthority("ROLE_" + usuario.getRole().getName().name()))
-        );
+        return new UsuarioDetails(usuario);
     }
 }
